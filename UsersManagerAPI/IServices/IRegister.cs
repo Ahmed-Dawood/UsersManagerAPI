20409cf31dd93cbca3b9ebf54201e220945d6267
Ahmed-Dawood <akdawood97@gmail.com>
2021-07-28 00:12:34 +0200

    Register User added

---
+++ UsersManagerAPI/IServices/IRegister.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using UsersManagerAPI.DomainClasses.Models;
+
+namespace UsersManagerAPI.IServices
+{
+    public interface IRegister
+    {
+        Task<UserInfo> SignUp(UserInfo loginInfo);
+        Task<string> ConfirmMail(string UserName);
+    }
+}
---
+++ UsersManagerAPI/Services/Register.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using UsersManagerAPI.DomainClasses.Models;
+using UsersManagerAPI.IServices;
+
+namespace UsersManagerAPI.Services
+{
+    public class Register : IRegister
+    {
+        UserInfo _loginInfo = new UserInfo();
+        public Task<string> ConfirmMail(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        async public Task<UserInfo> SignUp(UserInfo loginInfo)
+        {
+            _loginInfo = new UserInfo();
+            try
+            {
+                //LoginInfo loginInfo_ = await this.CheckRecordExistence(loginInfo);
+            }
+            catch (Exception ex)
+            {
+                _loginInfo.Messege = ex.Message;
+            }
+            return _loginInfo;
+        }
+
+        async public Task<bool> CheckRecordExistence(UserInfo loginInfo)
+        {
+            UserInfo loginInfo_ = new UserInfo();
+            if (!string.IsNullOrEmpty(loginInfo.UserName))
+            {
+                loginInfo_ = await this.GetLoginUser(loginInfo.UserName);
+            }
+            return true;
+        }
+        async public Task<UserInfo> GetLoginUser(string userName)
+        {
+            //UserInfo loginInfo = new UserInfo();
+            //using (IDbConnection connection = new SqlConnection(Global.ConnectionString))
+            //{
+            //    if (connection.State == ConnectionState.Closed) connection.Open();
+
+            //    string SqlCommand = "";
+
+            //    if (!string.IsNullOrEmpty(userName)) SqlCommand += " AND Username='" + userName + "'";
+
+            //};
+            return null;//loginInfo;
+        }
+    }
+}
