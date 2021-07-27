d1f6850745eb6be4297df9672c5bd7994ff632c7
Ahmed-Dawood <akdawood97@gmail.com>
2021-07-28 00:13:48 +0200

    USersDBContext Added

---
+++ UsersManagerAPI/DataAccess/UsersDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersManagerAPI.DomainClasses.Models;
+
+namespace UsersManagerAPI.DataAccess
+{
+    public class UsersBDContext : DbContext
+    {
+        public UsersBDContext(DbContextOptions<UsersBDContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserInfo> Users { get; set; }
+    }
+}
